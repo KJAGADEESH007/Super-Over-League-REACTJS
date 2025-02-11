3f12be605e1ac2768454dc544d6be4dba8e6cdb3
KJAGADEESH007 <karuturijagadesh@gmail.com>
2025-02-11 13:56:50 +0000

    ReactBasciccommit

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+  <div class='container'>
+    <h1>Super Over league</h1>
+    <div>
+      <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'></img>
+      <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
